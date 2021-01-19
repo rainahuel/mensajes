f6aab7e63d0da01837de4ae083b5bfff8c8915d8
Arturo Rainahuel <arainahuel@trebolit.lan>
2021-01-18 21:19:36 -0300

    se cambia archivo server.js

--- server.js
+++ server.js
@@ -8,14 +8,7 @@ const bodyParser = require('body-parser')
 
 const port = process.env.PORT || 3001; 
 
-app.use(cors(
-    {
-        "origin": "*",
-        "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-        "preflightContinue": false,
-        "optionsSuccessStatus": 204
-      }
-))
+app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 
